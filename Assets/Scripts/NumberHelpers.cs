78e9363df02afca537a2820298104a242c88bc89
Nebrok <nebrok.red@outlook.com>
2026-04-23 14:12:34 +0200

    Project engine version upgrade and worker upgrades implementation

--- Assets/Scripts/NumberHelpers.cs
+++ Assets/Scripts/NumberHelpers.cs
@@ -1,6 +1,15 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ScientficNumber
 {
+    private bool _isNegative = false;
+    private List<ushort> _digitList = new List<ushort>();
+
+
+
+
+
+
 
 }
--- Assets/Scripts/Worker.cs
+++ Assets/Scripts/Worker.cs
@@ -10,19 +10,23 @@ public class Worker : MonoBehaviour
 
     //UI
     [SerializeField] public Image BackgroundImage;
+    [SerializeField] public Image UpgradeBackgroundImage;
     [SerializeField] public GameObject NameTextObject;
     [SerializeField] public GameObject LevelTextObject;
     [SerializeField] public GameObject EarningsTextObject;
+    [SerializeField] public GameObject UpgradeTextObject;
     [SerializeField] public Slider ProgressBarObject;
 
 
     private Score _scoreRef;
     
     [SerializeField] private int _level = 0;
-    private float _baseAmount = 1;
-    private float _baseWorkTimeSeconds = 1.5f;
-    private float _workTimeSeconds;
+    [SerializeField] private float _baseAmount = 1;
+    [SerializeField] private float _baseWorkTimeSeconds = 1.5f;
 
+    [SerializeField] private float _nextUpgradeCost = 1.0f;
+    
+    private float _workTimeSeconds;
     private float _workTimeElapsed = 0.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -41,7 +45,7 @@ public class Worker : MonoBehaviour
             EarningsTextObject.GetComponent<TextMeshProUGUI>().text = (_baseAmount * _level).ToString();
         }
 
-        _scoreRef = FindFirstObjectByType<Score>();
+        _scoreRef = FindAnyObjectByType<Score>();
 
         _workTimeSeconds = _baseWorkTimeSeconds;
         if (_level <= 0)
@@ -53,6 +57,19 @@ public class Worker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (UpgradeTextObject != null)
+        {
+            UpgradeTextObject.GetComponent<TextMeshProUGUI>().text = _nextUpgradeCost.ToString();
+        }
+        if (_scoreRef.MainScore < _nextUpgradeCost)
+        {
+            UpgradeBackgroundImage.color = new Color(0.5f, 0.5f, 0.5f, 1.0f);
+        }
+        else
+        {
+            UpgradeBackgroundImage.color = new Color(0.2f, 1.0f, 0.3f, 1.0f);
+        }
+
         if (_level <= 0)
         {
             return;
@@ -63,6 +80,9 @@ public class Worker : MonoBehaviour
             WorkComplete();
         }
         ProgressBarObject.value = _workTimeElapsed / _workTimeSeconds;
+
+
+
     }
 
     void WorkComplete()
@@ -78,7 +98,15 @@ public class Worker : MonoBehaviour
 
     public void LevelUpWorker()
     {
+        if (_scoreRef.MainScore < _nextUpgradeCost)
+        {
+            return;
+        }
+
+        _scoreRef.MainScore -= _nextUpgradeCost;
+        _nextUpgradeCost *= 1.5f;
         _level += 1;
+
         BackgroundImage.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
         if (LevelTextObject != null)
         {
